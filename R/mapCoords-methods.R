c87c63e9253e0b182b035a53c077961aed176ca7
Peter Hickey <hickey@wehi.edu.au>
2014-11-18 04:39:25 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: 22280f1c5cd11f89ff5557c52787387b8d2a9a6e
    
        Updated [p]mapCoords
    
    Committed by: Peter Hickey
    Author Name: Peter Hickey
    Commit date: 2014-11-18 15:38:49 +1100
    Author date: 2014-11-18 15:38:49 +1100
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicTuples@96842 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mapCoords-methods.R
+++ R/mapCoords-methods.R
@@ -1,14 +1,38 @@
 ### =========================================================================
-### mapCoords methods
+### [p]mapCoords methods
 ### -------------------------------------------------------------------------
 ###
 
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### mapCoords
+###
 #' @export
 setMethod("mapCoords", 
           c("GTuples", "GTuplesList"), 
-          function(from, to, ..., ignore.strand = FALSE, elt.loc = FALSE, 
-                   elt.hits = FALSE) {
-            stop(paste0(class(from), " do not currently support the 'mapCoords' ", 
-                        "method."))
+          function(from, to, ..., ignore.strand = TRUE, elt.hits = FALSE) {
+            stop(paste0(class(from), " do not currently support the ", 
+                        "'mapCoords' method."))
+          }
+)
+
+#' @export
+setMethod("mapCoords", 
+          c("GTuples", "GTuples"), 
+          function(from, to, ..., ignore.strand = TRUE, elt.hits = FALSE) {
+            stop(paste0(class(from), " do not currently support the ", 
+                        "'mapCoords' method."))
+          }
+)
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### pmapCoords
+###
+
+#' @export
+setMethod("pmapCoords", 
+          c("GTuples", "GTuplesList"), 
+          function(from, to, ..., ignore.strand = TRUE, elt.hits = FALSE) {
+            stop(paste0(class(from), " do not currently support the ", 
+                        "'pmapCoords' method."))
           }
 )
