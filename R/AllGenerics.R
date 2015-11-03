5f64df5591d4e791185fc020e61ceff8da7b1611
Peter Hickey <hickey@wehi.edu.au>
2015-11-03 01:28:51 +0000

    Merge branch 'master' into devel
    * master:
      Add gtuples() and gtlist() generics and documentation (closes #16).
      Fix typo in README
      Fix typo in README
      Add AppVeyor support (closes #25) and update README (closes #28)
      Fixed by git SVN integreation fuckup by using a suitably big hammer (closes #20)
      Fixed by git SVN integreation fuckup by using a suitably big hammer (closes #20)
      Test git svn integration
      Test git svn integration
      Merge branch 'master' into devel
    
    From: Peter Hickey <peter.hickey@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicTuples@110243 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -38,3 +38,35 @@ setGeneric("tuples", function(x) {
 setGeneric("tuples<-", function(x, ..., value) {
   standardGeneric("tuples<-")
 })
+
+### -------------------------------------------------------------------------
+### gtuples
+###
+
+# Similar to granges, i.e., it "squeezes" the GTuples out of a tuples-based 
+# object.
+# NOTE: No methods are implemented in the GenomicTuples package but might be 
+#       implemented in a package for which there is a class that uses a 
+#       GTuples object as a slot.
+#' @importFrom methods setGeneric
+#' 
+#' @export
+setGeneric("gtuples", function(x, use.mcols = FALSE, ...) {
+  standardGeneric("gtuples")
+})
+
+### -------------------------------------------------------------------------
+### gtlist
+###
+
+# Similar to grglist, i.e., it "squeezes" the GTuplesList out of a tuples-based 
+# object.
+# NOTE: No methods are implemented in the GenomicTuples package but might be 
+#       implemented in a package for which there is a class that uses a 
+#       GTuplesList object as a slot.
+#' @importFrom methods setGeneric
+#' 
+#' @export
+setGeneric("gtlist", function(x, use.mcols = FALSE, ...) {
+  standardGeneric("gtlist")
+})
