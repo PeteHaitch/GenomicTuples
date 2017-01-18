a8c16c435c6a8ed3a8fcfeb0d777d0830d3d0c2a
Peter Hickey <hickey@wehi.edu.au>
2017-01-18 00:46:31 +0000

    testthat::not() is deprecated
    From: Peter Hickey <peter.hickey@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenomicTuples@125987 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test-GTuples-class.R
+++ tests/testthat/test-GTuples-class.R
@@ -8,7 +8,7 @@ context("GTuples validity methods")
 
 test_that(".valid.GTuples.pos works for 1-tuples", {
   expect_that(GTuples('chr1', tuples = matrix(12:1, ncol = 1)), 
-              not(throws_error()))
+              negate(throws_error()))
 })
 test_that(".valid.GTuples.pos works for 2-tuples", {
   expect_error(GTuples('chr1', tuples = matrix(12:1, ncol = 2)), 
