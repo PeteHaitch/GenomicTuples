3bd1c6ac4d40f402eadd2783421f4f617cc82694
Peter Hickey <peter.hickey@gmail.com>
2021-04-29 08:37:15 +1000

    Fix failing unit test
    - Update expected error message

--- tests/testthat/test-GTuples-class.R
+++ tests/testthat/test-GTuples-class.R
@@ -11,22 +11,25 @@ test_that(".valid.GTuples.pos works for 1-tuples", {
               not(throws_error()))
 })
 test_that(".valid.GTuples.pos works for 2-tuples", {
-  expect_error(GTuples('chr1', tuples = matrix(12:1, ncol = 2)), 
-               "In range 1: 'end' must be >= 'start' - 1.")
+  expect_error(
+    GTuples('chr1', tuples = matrix(12:1, ncol = 2)), 
+    "each range must have an end that is greater or equal to its start minus\n  one")
   expect_error(GTuples('chr1', tuples = cbind(11:20, 11:20)), 
                "positions in each tuple must be sorted")
   expect_error(GTuples('chr1', tuples = cbind(11:20, c(12:20, 20L))), 
                "positions in each tuple must be sorted")
 })
 test_that(".valid.GTuples.pos works for m-tuples (m > 2)", {
-  expect_error(GTuples('chr1', tuples = matrix(12:1, ncol = 3)), 
-               "In range 1: 'end' must be >= 'start' - 1.")
+  expect_error(
+    GTuples('chr1', tuples = matrix(12:1, ncol = 3)), 
+    "each range must have an end that is greater or equal to its start minus\n  one")
   expect_error(GTuples('chr1', tuples = cbind(11:20, 1:10, 31:40)), 
                "positions in each tuple must be sorted")
   expect_error(GTuples('chr1', tuples = cbind(11:20, c(12:20, 20L), 31:40)), 
                "positions in each tuple must be sorted")
-  expect_error(GTuples('chr1', tuples = matrix(12:1, ncol = 4)), 
-               "In range 1: 'end' must be >= 'start' - 1.")
+  expect_error(
+    GTuples('chr1', tuples = matrix(12:1, ncol = 4)), 
+    "each range must have an end that is greater or equal to its start minus\n  one")
   expect_error(GTuples('chr1', tuples = cbind(11:20, 1:10, 31:40, 41:50)), 
                "positions in each tuple must be sorted")
   expect_error(GTuples('chr1', tuples = cbind(11:20, 31:40, 1:10, 41:50)), 
